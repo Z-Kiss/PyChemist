68f76f46bec12c224021621c793ba0b3564e0aa2
zotya <zoltankiss9210@gmail.com>
2023-05-08 15:48:28 +0200

    register Potion, Ingredient, Recipe model

--- PyChemist/website/admin.py
+++ PyChemist/website/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Potion, Recipe, Ingredient
 
 # Register your models here.
+
+admin.site.register(Potion)
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
